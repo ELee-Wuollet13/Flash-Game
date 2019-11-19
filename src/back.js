ef863a412d16b678b962a238c88a0c849081aaf5
Student <example@example.com>
2019-11-19 15:32:39 -0800

    Working on UI

--- src/back.js
+++ src/back.js
@@ -1,97 +1,36 @@
-export { UserName }
+export { UserName };
 
 
 class  UserName {
   constructor(username) {
     this.userName = username;
     this.currentScore = 0;
-    this.listA = ["q1", "q2", "q3", "q4", "q5"];
-    this.listB = ["a1", "a2", "a3", "a4", "a5"];
-    this.flashCard = [];
+    this.listA = [".toString", "toUppercase", "parseInt", "isNaN", ".slice"];
+    this.listB = ["Converts a number to a string format", "Converts words to to uppercase", "Converts a string to a number format", "Checks if a number is 'nan'", "Removes or moves part of a number or strong at current position"];
+
   }
   randomGenerator() {
     return Math.floor(Math.random()*this.listA.length);
+    
   }
   newQuestion() {
+    this.flashCard = [];
     let newQuestion = this.randomGenerator();
-    console.log(newQuestion);
     this.flashCard.push(this.listA[newQuestion]);
     this.flashCard.push(this.listB[newQuestion]);
-    // flashCard.push(this.listB[newQuestion]);
-    // flashCard.push(this.listB[newQuestion + 1]);
-    // flashCard.push(this.listB[newQuestion + 2]);
-
-    //now loop for false listB
+    this.flashCard.push(this.listB[newQuestion + 1]);
+    this.flashCard.push(this.listB[newQuestion + 2]);
     return this.flashCard;
   }
-
+  answerQuestion(playerChoice){
+    if (playerChoice == 1) {
+      this.currentScore += 1;
+      console.log(this.currentScore);
+      //trigger new card
+    } else {
+      console.log("Wrong Answer");
+      //trigger new card
+      return this.currentScore;
+    }
+  }
 }
-
-
-
-// let test = new UserName("john");
-// console.log(test.newQuestion());
-// test.newQuestion();
-
-
-
-
-
-
-// let testBuild = new UserName("Everett");
-// let showMe = testBuild.newQuestion();
-
-
-// class FlashGame {
-  //
-  //   let wrongAnswerArray = [];
-  //
-  //
-  //   let question = randomGenerator();
-  //
-  //   //logic to get right questionn from index array
-  //   //logic to cross reference array return to object Q&A pair
-  //   for (i=0; i < 3; i++) {
-    //   let wrongAnswer = randomGenerator();
-    //   if (wrongAnswer === question) {
-      //     wrongAnswer = randomGenerator();
-      //     wrongAnswerArray.push(wrongAnswer); //this has a problem, no reeval
-      //   } else {
-        //     wrongAnswerArray.push(wrongAnswer);  //create wrongAnswerArray
-        //   };
-        // };
-        // };
-        //
-        // const q1 = card1 {
-          //   question: ".toString";
-          //   answer: "Converts a number to a string format";
-          // }
-          //
-          // const q2 = card2 {
-            //   question: "toUppercase";
-            //   answer: "Converts workds to to uppercase";
-            // }
-            //
-            // const q3 = card3 {
-              //   question: ".parseInt";
-              //   answer: "Converts a string to a number format";
-              // }
-              //
-              // const q4 = card4 {
-                //   question: "inNaN";
-                //   answer: "Checks if a number is 'nan'";
-                // }
-                //
-                // const q5 = card5 {
-                  //   question: ".slice";
-                  //   answer: "Removes or moves part of a number or strong at current position";
-                  // }
-
-                  //
-                  //
-                  //
-                  // const questions =["q1", "q2", "q3", "q4", "q5"];
-                  // function randomGenerator() {
-                    //   return [Math.floor(Math.random()*questions.length)];
-                    // }
-                    //   let question = questions[Math.floor(Math.random()*questions.length)];
--- src/main.js
+++ src/main.js
@@ -8,10 +8,31 @@ import './styles.css';
 $(document).ready(function(){
   $('form#user').submit(function(event){
     event.preventDefault();
+    $(".onSubmit").show();
+    $("#user").hide();
     const username = $('#intake').val();
     $(".nameDisplay").text(username);
     let card = new UserName(username);
-    let card2 = card.newQuestion();
-    $("#cardDisplay").text(card.flashCard);
+    let output = card.newQuestion();
+    let question = output[0];
+    $("#question").text(question);
+    let answer = output[1];
+    $("#question1").text(answer);
+    let answer2 = output[2];
+    $("#question2").text(answer2);
+    let answer3 = output[3];
+    $("#question3").text(answer3);
+    $("form#card").submit(function(event) {
+      event.preventDefault();
+      let playerChoice = $("input:radio[name=radio1]:checked").val();
+      card.answerQuestion(playerChoice);
+      $('#outputScore').text(card.currentScore);
+      card.newQuestion();
+      console.log(card.flashCard);
+
+    });
   });
+  // $('form#card').onClick(function(event) {
+  //   event.preventDefault();
+//   });
 });
